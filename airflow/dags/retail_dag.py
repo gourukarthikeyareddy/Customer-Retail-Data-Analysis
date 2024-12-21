d5bd9d6be2ac5589a032fda71318e7679c9f280e
Karthikeya Reddy Gouru <42973884+gourukarthikeyareddy@users.noreply.github.com>
2024-12-21 10:24:45 -0500

    Update retail_dag.py

--- airflow/dags/retail_dag.py
+++ airflow/dags/retail_dag.py
@@ -2,7 +2,13 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -20,5 +26,34 @@ def retail():
         gcp_conn_id='gcp',
         mime_type='text/csv',
     )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://marclamberti_online_retail/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    check_load()    
 
 retail()
